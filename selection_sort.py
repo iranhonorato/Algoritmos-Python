149c0feebeb1b50bd4382509669e461cdf70026b
iran <iranilsonhonorato88@gmail.com>
2025-09-10 08:19:32 -0300

    feat(algorithm): Implementando algoritmo de selection sort

---
+++ selection_sort.py
@@ -0,0 +1,11 @@
+def selection_sort(lista:list) -> list:
+    for i in range(len(lista)):
+        min_index = i 
+
+        for j in range(min_index+1, len(lista)):
+            if lista[j] < lista[min_index]:
+                lista[j], lista[min_index] = lista[min_index], lista[j]
+
+    return lista
+
+
