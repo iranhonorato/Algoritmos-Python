9768b8634d403f944bf91fa313caebab86b66d18
iran <iranilsonhonorato88@gmail.com>
2025-09-22 14:13:13 -0300

    feat(algorithm): Implementando algoritmo quicksort

--- quick_sort.py
+++ quick_sort.py
@@ -1,4 +1,4 @@
-def particiona(inicio:int, lista:list, fim:int):
+def particiona(lista:list, inicio:int, fim:int) -> int:
     lp = inicio
     pivo = lista[inicio]
     count = inicio + 1
@@ -9,9 +9,20 @@ def particiona(inicio:int, lista:list, fim:int):
         count += 1
 
     lista[lp], lista[inicio] = lista[inicio], lista[lp]
+    return lp
+
+
+
+
+def quick_sort(lista:list, inicio:int, fim:int):
+    if inicio < fim:
+        lp = particiona(lista, inicio, fim)
+        quick_sort(lista, inicio, lp)
+        quick_sort(lista, lp+1, fim)
     return lista
 
-# 
 
 lista = [5,1,7,2,3,6,4]
-print(particiona(0, lista, len(lista)))
+print(quick_sort(lista,0,len(lista)))
+
+
--- selection_sort.py
+++ selection_sort.py
@@ -7,5 +7,5 @@ def selection_sort(lista:list) -> list:
                 lista[j], lista[min_index] = lista[min_index], lista[j]
 
     return lista
-
-
+lista = [5,1,7,2,3,6,4]
+print(selection_sort(lista))
