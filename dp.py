461c519c2cef810d2c6418b504c31ab4f81dca8e
iran <iranilsonhonorato88@gmail.com>
2025-10-13 12:54:09 -0300

    fix: Consertando fibonnaci

--- dp.py
+++ dp.py
@@ -1,16 +1,17 @@
-def fib_memo(n, memo=None):
-    if memo is None:
-        memo = {}
+def finbonacci_dp(n, memo=None):
+    if memo == None: 
+        memo = [-1 for i in range(n)]
 
-    if n in memo:
-        return memo[n]
-
-    if n <= 1:
-        memo[n] = n
+    if n < 2 or n == 2:
+        memo[n-1] = 1 
+        return memo[n-1]
+    
+    if memo[n-1] == -1:
+        memo[n-1] = finbonacci_dp(n-1, memo) + finbonacci_dp(n-2, memo)
+        return memo[n-1]
+    
     else:
-        memo[n] = fib_memo(n-1, memo) + fib_memo(n-2, memo)
+        return memo[n-1]
+        
 
-    return memo[n]
-
-
-print(fib_memo(10)) 
+print(finbonacci_dp(100))
