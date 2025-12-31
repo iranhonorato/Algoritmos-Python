e71d1fc184ebe50823e36390c5b450a6a025f1c7
iran <iranilsonhonorato88@gmail.com>
2025-12-30 22:51:07 -0300

    feat: Implementando linked list

--- data strucutures/singly_linked_list.py
+++ data strucutures/singly_linked_list.py
@@ -1,33 +1,38 @@
 # Estrutura do nó
 class Node:
     """
-    each node stores a reference 
+    Each node stores a reference 
     to an object that is an element of the sequence
     """
 
-    def __init__(self, value):    
-        self.value = value 
+    def __init__(self, data):    
+        self.data = data 
         self.next = None
 
 
 
+def add_first(head:Node, data) -> Node:
+    """Insert a new node at the beginning of the linked list"""
+    new_node = Node(data)
+    new_node.next = head
+    return new_node
 
 
-# Estrutua da singly linked list
-class SinglyLinkedList:
-    def __init__(self):
-        self.head = None 
-        self.size = 0
+def show_linked_list(head:Node) -> None:
+    """Traverse the linked list and print its elements"""
+    current = head
+    while current:
+        print(current.data, end=" -> ")
+        current = current.next
+    print("None")
 
-    def add_first(self, value):
-        """
-        add_first(10)
-        [10] → None
 
-        add_first(5)
-        [5] → [10] → None
-        """
-        node = Node(value)
-        node.next = self.head
-        self.head = node 
-        self.size += 1 
+
+node = Node(5)
+node = add_first(node, 4)
+node = add_first(node, 3)
+node = add_first(node, 2)
+node = add_first(node, 1)
+show_linked_list(node)
+
+
